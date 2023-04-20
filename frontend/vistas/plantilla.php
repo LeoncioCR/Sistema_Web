a0ecb19a03c05740b9d9d1fe1fd649c62adc02f5
LeoncioCR <leocuevaruiz@gmail.com>
2023-04-20 11:14:12 -0500

    Primera version frontend

---
+++ frontend/vistas/plantilla.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimun-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+	<meta name="title" content="Tienda Virtual">
+
+	<meta name="description" content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat aspernatur voluptas velit commodi, libero. Nemo, soluta facilis minima tempora at similique tenetur placeat eius">
+
+	<meta name="keyword" content="Lorem ipsum, dolor sit amet consectetur adipisicing elit, Placeat aspernatur voluptas velit commodi, libero Nemo, soluta facilis minima tempora at, similique tenetur placeat eius">
+
+	<title>Tienda Virtual</title>
+
+	<?php  
+
+		$icono = ControladorPlantilla::ctrEstiloPlantilla();
+
+		echo '<link rel="icon" href="http://localhost/backend/'.$icono["icono"].'">';
+
+		/*--====================================
+		   MANTENER LA RUTA FIJA DEL PROYECTO
+		======================================*/
+
+		$url = Ruta::ctrRuta();
+
+	?>
+
+	<link rel="stylesheet" href="<?php echo $url; ?>vistas/css/plugins/bootstrap.min.css">
+
+	<link rel="stylesheet" href="<?php echo $url; ?>vistas/css/plugins/font-awesome.min.css">
+
+	<link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+
+	<link href="https://fonts.googleapis.com/css?family=Ubuntu|Ubuntu+Condensed" rel="stylesheet">
+
+	<link rel="stylesheet" href="<?php echo $url; ?>vistas/css/plantilla.css">
+
+	<link rel="stylesheet" href="<?php echo $url; ?>vistas/css/cabezote.css">
+
+	<link rel="stylesheet" href="<?php echo $url; ?>vistas/css/slide.css">
+
+	<script src="<?php echo $url; ?>vistas/js/plugins/jquery.min.js"></script>
+
+	<script src="<?php echo $url; ?>vistas/js/plugins/bootstrap.min.js"></script>
+
+</head>
+<body>
+
+	<?php  
+
+	/*--====================================
+					  CABEZOTE
+	======================================*/
+
+	include "modulos/cabezote.php";
+
+	/*--====================================
+			   CONTENIDO DINÁMICO
+	======================================*/
+
+	$rutas = array();
+	$ruta = null;
+
+	if(isset($_GET["ruta"])){
+
+		$rutas = explode("/", $_GET["ruta"]);
+
+		$item = "ruta";
+		$valor = $rutas[0];
+
+		/*--====================================
+			   URL AMIGABLES DE CATEGORIAS
+		======================================*/
+
+		$rutaCategorias = ControladorProductos::ctrMostrarCategorias($item, $valor);
+
+		if($valor == $rutaCategorias["ruta"]){
+
+			$ruta = $rutas[0];
+
+		}
+
+		/*--====================================
+			 URL AMIGABLES DE SUBCATEGORIAS
+		======================================*/
+
+		$rutaSubCategorias = ControladorProductos::ctrMostrarSubCategorias($item, $valor);
+
+		foreach ($rutaSubCategorias as $key => $value) {
+			
+			if($valor == $value["ruta"]){
+
+			$ruta = $rutas[0];
+
+			}
+
+		}
+
+		/*--====================================
+			  LISTA BLANCA DE URL AMIGABLES
+		======================================*/
+
+		if($ruta != null){
+
+			include "modulos/productos.php";
+
+		}else{
+
+			include "modulos/error404.php";
+
+		}
+
+	}else{
+
+		include "modulos/slide.php";
+
+	}
+
+	?>
+
+	<script src="<?php echo $url; ?>vistas/js/cabezote.js"></script>
+
+	<script src="<?php echo $url; ?>vistas/js/plantilla.js"></script>
+
+</body>
+</html>
